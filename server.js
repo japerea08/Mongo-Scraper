b146008d8e0b11937a4a3ba51ff22aeea4f08940
Joey Perea <j.perea08@gmail.com>
2018-06-21 18:15:52 -0400

    changed the server.js to be able to connect remotely

--- server.js
+++ server.js
@@ -21,7 +21,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/articleScraper");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articleScraper";
+
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 //Routes
 app.get("/scrape", function(req,res){
